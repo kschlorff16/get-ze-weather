dff0eab879062652249e76b8b70018d807887823
kschlorff16 <12kschlorff@gmail.com>
2018-10-17 21:16:46 -0500

    Adding main and main_test, will probably extract some functions out to another file soon

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"dwolla/models"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"unicode"
+)
+
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?q="
+const imperialUnits = "&units=imperial"
+const appIDSuffix = "&APPID="
+const prompt = "Where are you?"
+
+func main() {
+	if os.Getenv("OPENWEATHERMAPAPIKEY") == "" {
+		fmt.Println("Whoa there. Make sure you set the OPENWEATHERMAPAPIKEY variable before you get started.")
+		os.Exit(1)
+	}
+	fmt.Println(prompt)
+	response := promptUserForInput()
+	cleanedResponse := cleanUserResponse(response)
+	if responseIsOk(cleanedResponse) {
+		fullURL := buildFullURL(cleanedResponse)
+		responseAsByteArray := convertHTTPResponseToByteArray(fullURL)
+		models.PrintLocationAndTemperatureInfo(responseAsByteArray)
+	}
+}
+
+func convertHTTPResponseToByteArray(fullURL string) []byte {
+	httpResponse, err := http.Get(fullURL)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer httpResponse.Body.Close()
+	httpResponseAsByteArray, _ := ioutil.ReadAll(httpResponse.Body)
+	if strings.Contains(string(httpResponseAsByteArray), "city not found") {
+		fmt.Println("That city wasn't found. Try again")
+		os.Exit(1)
+	}
+	return httpResponseAsByteArray
+}
+
+func promptUserForInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	if responseIsEmpty(text) {
+		fmt.Println("You must enter a value.")
+		os.Exit(1)
+	}
+	return strings.TrimSpace(text)
+}
+
+func cleanUserResponse(stringThatMayContainCommasOrSpaces string) string {
+	cleanedInput := stringThatMayContainCommasOrSpaces
+	if strings.Contains(stringThatMayContainCommasOrSpaces, ",") {
+		cleanedInput = splitStringOnComma(stringThatMayContainCommasOrSpaces)
+	}
+
+	if strings.Contains(stringThatMayContainCommasOrSpaces, " ") {
+		cleanedInput = splitStringOnSpace(stringThatMayContainCommasOrSpaces)
+	}
+
+	return cleanedInput
+}
+
+func responseIsOk(response string) bool {
+	cleanedInput := response
+
+	for _, character := range cleanedInput {
+		if !unicode.IsLetter(character) {
+			return false
+		}
+	}
+	return true
+}
+
+func responseIsEmpty(response string) bool {
+	if response == "" {
+		return true
+	}
+	return false
+}
+
+func buildFullURL(location string) string {
+	apiKey := os.Getenv("OPENWEATHERMAPAPIKEY")
+	apiStrings := []string{baseURL, location, imperialUnits, appIDSuffix, apiKey}
+	fmt.Println(strings.Join(apiStrings[:], ""))
+	return strings.Join(apiStrings[:], "")
+}
+
+func splitStringOnComma(stringThatContainsCommas string) string {
+	return strings.Split(stringThatContainsCommas, ",")[0]
+}
+
+func splitStringOnSpace(stringThatContainsSpaces string) string {
+	fmt.Println("This is what I got: " + stringThatContainsSpaces)
+	fmt.Printf("This is what I gave back: %v", strings.Split(stringThatContainsSpaces, " ")[0])
+	return strings.Split(stringThatContainsSpaces, " ")[0]
+}
---
+++ main_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestResponseIsEmpty(t *testing.T) {
+	emptyInput := ""
+	if !responseIsEmpty(emptyInput) {
+		t.Errorf("The string was not empty. The string's value was %v", emptyInput)
+	}
+	notEmptyInput := "Hello"
+	if responseIsEmpty(notEmptyInput) {
+		t.Errorf("The string was empty. The string's value was %v", notEmptyInput)
+	}
+}
+
+func TestResponseIsOkWithValidInputs(t *testing.T) {
+	validResponses := []string{"london", "Chicago", "Charleston"}
+
+	for _, validInput := range validResponses {
+		if !responseIsOk(validInput) {
+			t.Errorf("The input was not valid. The value that was invalid was %v.", validInput)
+		}
+	}
+}
+
+func TestResponseIsOkWithInvalidInputs(t *testing.T) {
+	invalidResponses := []string{"1l0v3c4ts"}
+
+	for _, validInput := range invalidResponses {
+		if responseIsOk(validInput) {
+			t.Errorf("The response was not valid. The value that was invalid was %v.", validInput)
+		}
+	}
+}
+
+func TestBuildFullURL(t *testing.T) {
+	input := "Testing"
+	os.Setenv("OPENWEATHERMAPAPIKEY", "definitelyanapikey")
+	const expected = "http://api.openweathermap.org/data/2.5/weather?q=Testing&units=imperial&APPID=definitelyanapikey"
+	actual := buildFullURL(input)
+	if actual != expected {
+		t.Errorf("The API URL was not built as expected.\n Expected: %v\n Actual: %v", expected, actual)
+	}
+}
+
+func TestSplitStringOnComma(t *testing.T) {
+	const beep = "beep,boop"
+	if splitStringOnComma(beep) != "beep" {
+		t.Errorf("The string was not split correctly. The expected value was beep, but got %v", splitStringOnComma(beep))
+	}
+}
+
+func TestSplitStringOnSpace(t *testing.T) {
+	const beep = "beep boop"
+	if splitStringOnSpace(beep) != "beep" {
+		t.Errorf("The string was not split correctly. The expected value was beep, but got %v", splitStringOnSpace(beep))
+	}
+}
+
+func TestCleanUserResponse(t *testing.T) {
+	userResponses := []string{"london", "Chicago", "Charleston"}
+
+	for _, response := range userResponses {
+		cleanedResponse := cleanUserResponse(response)
+		if strings.Contains(cleanedResponse, ",") || strings.Contains(cleanedResponse, " ") {
+			t.Errorf("The input was not valid. The value that was invalid was %v.", cleanedResponse)
+		}
+	}
+}
